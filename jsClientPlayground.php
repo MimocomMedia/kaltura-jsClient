aefc8f3d8e69877240c9e842ad8592b5c6997dd0
kalturian <kalturian@b58a29cf-3064-46da-94c6-1c29cc75c8e5>
2010-01-05 09:57:14 +0000

    git-svn-id: http://www.kaltura.org/kalorg/kcl_js/trunk/generator/output/jsClient@430 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- jsClientPlayground.php
+++ jsClientPlayground.php
@@ -3,13 +3,20 @@
 		<title>JavaScript client library sandbox</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/combine.php?type=javascript&files=KalturaJsUtils.js,KalturaClientBase.js,KalturaTypes.js,KalturaVO.js,KalturaClient.js,KalturaServices.js,ox.ajast.js"></script>
+		<!-- <script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/combine.php?type=javascript&files=KalturaJsUtils.js,KalturaClientBase.js,KalturaTypes.js,KalturaVO.js,KalturaClient.js,KalturaServices.js,ox.ajast.js"></script> -->
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaJsUtils.js"></script>
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaClientBase.js"></script>
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaTypes.js"></script>
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaVO.js"></script>
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaClient.js"></script>
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaServices.js"></script>
+		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/ox.ajast.js"></script>
 		<script type="text/javascript">
 			/**
 			* Prepare the JQuery for cross-domain ajax requests via flXHRproxy and register the ajax start and end events to the loading gif.
 			*/
 			function onerrorH (info) {
-				console.log("Errorrr: ", onerrorH)
+				console.log("Error: ", onerrorH)
 			}
 		</script>	
 	</head>
@@ -34,7 +41,20 @@
 			function sessionStarted(success, data) {
 				kClient.setKs(data);
 				writeLog('new KS created: ' + data, data);
-				kClient.media.listAction(getEntrieListHandler);
+				listEntries();
+			}
+
+			function listEntries() {
+				var entryFilter = new KalturaBaseEntryFilter();
+				console.log(getClass(entryFilter));
+				if ($('#inputType').val() != '')
+					entryFilter.typeEqual = $('#inputType').val();
+				entryFilter.statusEqual = KalturaEntryStatus.READY;
+				entryFilter.orderBy = KalturaBaseEntryOrderBy.CREATED_AT_DESC;
+				var kalturaPager = new KalturaFilterPager();
+				kalturaPager.pageSize = 10;
+				kalturaPager.pageIndex = $('#inputPageNum').val();
+				kClient.baseEntry.listAction(getEntrieListHandler, entryFilter, kalturaPager);
 			}
 			function getEntrieListHandler (success, data) {
 				writeLog('List entries:' + data, data);
@@ -42,6 +62,8 @@
 			
 		</script>
 		<a href="#" onclick="startSession();return false;">Start session!</a>
+		<label for="inputPageNum">Page number: </label><input id="inputPageNum" value="1" />
+		<label for="inputType">Entry type: </label><input id="inputType" />
 		<!-- <img style="border:none; " src="/flxhr/ajax-loader.gif" id="loadingImg"/> -->
 	</body>
 </html>
