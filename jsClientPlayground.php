23ce7c068cddbaa5a15a96bacf23cc6c5fe501e7
kalturian <kalturian@b58a29cf-3064-46da-94c6-1c29cc75c8e5>
2010-01-06 12:22:10 +0000

    playground grid
    git-svn-id: http://www.kaltura.org/kalorg/kcl_js/trunk/generator/output/jsClient@437 b58a29cf-3064-46da-94c6-1c29cc75c8e5

--- jsClientPlayground.php
+++ jsClientPlayground.php
@@ -11,6 +11,10 @@
 		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaTypes.js"></script>
 		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaVO.js"></script>
 		<script type="text/javascript" src="http://dev.kcl_js/generator/output/jsClient/js/KalturaServices.js"></script>
+		
+		<link rel="stylesheet" type="text/css" href="playground_libs/flexigrid/css/flexigrid/flexigrid.css">
+		<script type="text/javascript" src="playground_libs/flexigrid/flexigrid.js"></script>
+
 		<script type="text/javascript">
 			/**
 			* Prepare the JQuery for cross-domain ajax requests via flXHRproxy and register the ajax start and end events to the loading gif.
@@ -27,7 +31,7 @@
 		<script type="text/javascript">
 			//utility for output
 			function writeLog(msg, raw) {
-				$('#outputBlock').append(msg+'<br />');
+				//$('#outputBlock').append(msg+'<br />');
 				console.log(raw);
 			}
 
@@ -44,26 +48,95 @@
 				listEntries();
 			}
 
-			function listEntries() {
+			function listEntries(page, filterKey, filterValue, success, error) {
 				var entryFilter = new KalturaBaseEntryFilter();
 				if ($('#inputType').val() != '')
 					entryFilter.typeEqual = $('#inputType').val();
+				if (filterKey && filterKey != '')
+					entryFilter[filterKey] = filterValue;
 				entryFilter.statusEqual = KalturaEntryStatus.READY;
 				entryFilter.orderBy = KalturaBaseEntryOrderBy.CREATED_AT_DESC;
 				var kalturaPager = new KalturaFilterPager();
 				kalturaPager.pageSize = 10;
-				kalturaPager.pageIndex = $('#inputPageNum').val();
+				kalturaPager.pageIndex = page;
 				kClient.baseEntry.listAction(getEntrieListHandler, entryFilter, kalturaPager);
 			}
 			function getEntrieListHandler (success, data) {
-				writeLog('List entries:' + data, data);
+				var p = currentPage;
+				writeLog('List entries: ', data);
+				var d = buildGridDataProvider(p, data.totalCount, data.objects, 'id', ['id', 'name', 'tags', 'thumbnailUrl']);
+				writeLog('Grid populate: ' + d, d);
+				flexirep.flexAddData(d);
+			}
+
+			var gridData = null;
+			function buildGridDataProvider (pageNumber, totalPages, dataItems, idfield, fields) {
+				gridData = {
+					page: parseInt(pageNumber),
+					total: parseInt(totalPages),
+					rows: []
+				};
+				var data = {};
+				var key = '';
+				for (var i = 0; i < dataItems.length; ++i) {
+					data = dataItems[i];
+					gridData.rows.push ({id:data[idfield], cell:[]});
+					if (!fields) {
+						for(key in data) {
+							gridData.rows[i].cell.push(data[key]);
+						}
+					} else {
+						for (var j = 0; j < fields.length; ++j) {
+							key = fields[j];
+							gridData.rows[i].cell.push(data[key]);
+						}
+					}
+				}
+				return gridData;
+			}
+
+			var currentPage = 1;
+			function getDataProvider (data, success, error) {
+				currentPage = parseInt(data[0].value);
+				var filterValue = data[4].value;
+				var filterKey = data[5].value;
+				writeLog('get new data: ', data);
+				if (kClient.ks) {
+					listEntries(currentPage, filterKey, filterValue, success, error);
+				} else {
+					startSession();
+				}
 			}
 			
 		</script>
-		<a href="#" onclick="startSession();return false;">Start session!</a>
-		<a href="#" onclick="listEntries();return false;">List Baby!</a>
-		<label for="inputPageNum">Page number: </label><input id="inputPageNum" value="1" />
-		<label for="inputType">Entry type: </label><input id="inputType" />
-		<!-- <img style="border:none; " src="/flxhr/ajax-loader.gif" id="loadingImg"/> -->
+		<table id="flex1" style="display:none"></table>
+		<script type="text/javascript">
+			var flexirep = $("#flex1").flexigrid
+			({
+				dataType: 'json',
+				url: 'a',
+				colModel : [
+					{display: 'Id', name : 'id', sortable : true, align: 'center', width:'80'},
+					{display: 'Name', name : 'name', sortable : true, align: 'center', width:'160'},
+					{display: 'Tags', name : 'tags', sortable : true, align: 'center', width:'120'},
+					{display: 'Thumbnail', name : 'thumbnailUrl', sortable : true, align: 'center', width:'200'}
+					],
+				searchitems : [
+					{display: 'Id', name : 'idIn', isdefault: true},
+					{display: 'Name', name : 'nameLike'},
+					{display: 'Entry Type', name : 'typeEqual'}
+					],
+				sortname: "name",
+				sortorder: "asc",
+				usepager: true,
+				showTableToggleBtn: true,
+				title: 'Entries',
+				useRp: false,
+				rp: 10,
+				width: 700,
+				height: 200,
+				getDataProvider: getDataProvider
+			});			
+		</script>
 	</body>
 </html>
